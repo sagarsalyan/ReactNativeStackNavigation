c129b8fad6d95535760bbf0af04032182ab5a50a
sagarsalyan <sagarslyn@gmail.com>
2020-04-20 10:40:10 +0530

    Added more info

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Home from './components/Home';
@@ -7,6 +7,15 @@ import Search from './components/Search';
 import About from './components/About';
 
 const Stack = createStackNavigator();
+
+function LogoTitle() {
+  return (
+    <Image
+      style={{ width: 50, height: 50 }}
+      source={require('./images/checked.png')}
+    />
+  );
+}
 
 function App() {
   return (
@@ -34,7 +43,20 @@ function App() {
 
          }} />
         <Stack.Screen name="Search" component={Search} options={({ route }) => ({ title: route.params.name })}/>
-        <Stack.Screen name="About" component={About} />
+        <Stack.Screen
+          name="About"
+          component={About}
+          options={{
+            headerTitle: props => <LogoTitle {...props} />,
+            headerRight: () => (
+              <Button
+                onPress={() => alert('This is a button!')}
+                title="Info"
+                color="#fff"
+              />
+            ),
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
